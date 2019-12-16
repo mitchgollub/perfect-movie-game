95225d5dc23107f4b6e2b0e9c06deb757deae0fc
mitchgollub <mgollub@live.com>
2019-12-15 22:58:23 -0500

    Got it working!

---
+++ pages/api/getScore.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export default async (req, res) => {
+    try {
+        const movieName = req.body.movie;
+        console.log(movieName);
+
+        if (!movieName)
+            return res.status(400).json({ error: "'movie' parameter required" });
+
+        // TO-DO: Implement DB cache for input -> score
+        // Call API if response not in cache
+
+        // Call omdb api w/ movie name
+        const response = await axios.get(`https://www.omdbapi.com/?t=${encodeURI(movieName)}&apikey=${process.env.OMDB_API_KEY}`);
+        console.log(response.data);
+
+        // retrieve rotten tomatoes rating
+        const score = response.data.Ratings.find((rating) => rating.Source === "Rotten Tomatoes").Value.replace(/\D/g, '');
+        console.log(score);
+
+        return res.status(200).json({ score: score });
+
+    }
+    catch (error) {
+        return res.status(500).json({ error: error });
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,89 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+export default class Home extends Component {
+  constructor(props) {
+    super(props)
 
-    <Nav />
+    this.state = {
+      movie1: '',
+      movie2: '',
+      movie1Score: '__',
+      movie2Score: '__',
+      movie2Ready: false
+    }
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+    this.movie1OnChange = this.movie1OnChange.bind(this);
+    this.movie2OnChange = this.movie2OnChange.bind(this);
+    this.handleMovieEntry = this.handleMovieEntry.bind(this);
+  }
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
+  movie1OnChange(event) { this.setState({ movie1: event.target.value }) }
+  movie2OnChange(event) { this.setState({ movie2: event.target.value }) }
 
-    <style jsx>{`
+  async handleMovieEntry(event) {
+    const id = event.target.id;
+
+    let submit = this.state.movie2;
+
+    if (!this.state.movie2)
+      submit = this.state.movie1;
+
+    const res = await fetch(`/api/getScore`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        movie: submit
+      })
+    });
+    const score = (await res.json()).score;
+
+    let update = { movie1Score: score + '%' };
+
+    if (!(id === 'movie1Submit')) {
+      update = { movie2Score: score + '%' };
+    }
+
+    update.movie2Ready = true;
+
+    this.setState(update)
+  }
+
+  calculateScore() {
+    return Number(this.state.movie1Score.replace(/\D/g,'')) + Number(this.state.movie2Score.replace(/\D/g,'')) + '%';
+  }
+
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className="hero">
+          <h1 className="title">Welcome to The Perfect Movie Game!</h1>
+          <p className="description">
+            You'll choose two movies.  We'll add the Rotten Tomatoes Movie Scores together to see if you can hit 100% without going over.
+            Enter the name of a movie to get started!
+          </p>
+
+          <div className="row">
+            <h2>Movie 1: </h2><input type="text" name="movie1" disabled={this.state.movie2Ready} onChange={this.movie1OnChange} value={this.state.movie1} />
+            <button id="movie1Submit" onClick={this.handleMovieEntry} disabled={this.state.movie2Ready}>Submit</button>
+            <h2>Movie 2: </h2><input type="text" name="movie2" disabled={!this.state.movie2Ready} onChange={this.movie2OnChange} value={this.state.movie2} />
+            <button id="movie2Submit" onClick={this.handleMovieEntry} disabled={!this.state.movie2Ready}>Submit</button>
+          </div>
+
+          <div className="row">
+            <h1>{this.state.movie1Score} + {this.state.movie2Score} = {this.state.movie2Score !== '__' ? this.calculateScore() : '?'}</h1>
+          </div>
+        </div>
+
+        <style jsx>{`
       .hero {
         width: 100%;
         color: #333;
@@ -59,30 +106,8 @@ const Home = () => (
         flex-direction: row;
         justify-content: space-around;
       }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
     `}</style>
-  </div>
-)
-
-export default Home
+      </div>
+    )
+  };
+}
